c2594335b66013a8d4b217ef7a5cf089daf922b0
dscherpf <dscherpf@users.noreply.github.com>
2021-06-29 04:19:23 +0000

    deploy: 0bb9a45dc1c1aab30e8b986df91758b1ac4f9083

--- _nuxt/static/1624849650/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1624849650",layout:"default",error:a,state:{storeLink:"https:\u002F\u002Fwww.etsy.com\u002Fhollow-note"},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"cab8b680"}}}}(null,"\u002F"));
